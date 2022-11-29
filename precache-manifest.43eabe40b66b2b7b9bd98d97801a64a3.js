fbf8861c0ec8e3767205de5d5a9cc3343146bcf0
unknown <vivekkhodade01@gmail.com>
2022-11-30 00:51:13 +0530

    Updates

---
+++ precache-manifest.43eabe40b66b2b7b9bd98d97801a64a3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcc6314308c77c37b583",
+    "url": "/rohanwedsprashansa/static/css/main.2b8da5fd.chunk.css"
+  },
+  {
+    "revision": "bcc6314308c77c37b583",
+    "url": "/rohanwedsprashansa/static/js/main.bcc63143.chunk.js"
+  },
+  {
+    "revision": "c398fa23b22dbc5fc010",
+    "url": "/rohanwedsprashansa/static/js/runtime~main.c398fa23.js"
+  },
+  {
+    "revision": "bf9e0fb5bcc728abd7cf",
+    "url": "/rohanwedsprashansa/static/css/2.93074e24.chunk.css"
+  },
+  {
+    "revision": "bf9e0fb5bcc728abd7cf",
+    "url": "/rohanwedsprashansa/static/js/2.bf9e0fb5.chunk.js"
+  },
+  {
+    "revision": "ec959cf2e3c84958206aaf72608ebdb4",
+    "url": "/rohanwedsprashansa/static/media/background2.ec959cf2.jpg"
+  },
+  {
+    "revision": "dda1d79f3aef9457f875ae113934d455",
+    "url": "/rohanwedsprashansa/static/media/dp.dda1d79f.jpeg"
+  },
+  {
+    "revision": "9e050e136093bf73676534ea3f3a8b15",
+    "url": "/rohanwedsprashansa/static/media/ganapati.9e050e13.jpeg"
+  },
+  {
+    "revision": "cdee0b035e5f33636e9dc12ac0c427e6",
+    "url": "/rohanwedsprashansa/static/media/logo.cdee0b03.jpg"
+  },
+  {
+    "revision": "2725ee23af3c8073ab3c3c6e21e39198",
+    "url": "/rohanwedsprashansa/static/media/second.2725ee23.jpg"
+  },
+  {
+    "revision": "090db8682aac3fdf4aabd804cea22ef8",
+    "url": "/rohanwedsprashansa/static/media/hotel.090db868.jpg"
+  },
+  {
+    "revision": "18af806bc1de3249ab370f8b81eb9d02",
+    "url": "/rohanwedsprashansa/static/media/SANGEET.18af806b.gif"
+  },
+  {
+    "revision": "51d5069a4a95e8e4a9a48befda72392a",
+    "url": "/rohanwedsprashansa/static/media/carnival.51d5069a.gif"
+  },
+  {
+    "revision": "b67d985f84e28c9d0b31683072e96e81",
+    "url": "/rohanwedsprashansa/static/media/Nikasi.b67d985f.gif"
+  },
+  {
+    "revision": "03c2984ed7b36ed9ce08177b312017b3",
+    "url": "/rohanwedsprashansa/static/media/Phera.03c2984e.gif"
+  },
+  {
+    "revision": "d9eee02b07d291eb097c413c1d28b04d",
+    "url": "/rohanwedsprashansa/static/media/awesome.d9eee02b.gif"
+  },
+  {
+    "revision": "fc32523a85113ebe4f1be6485bc418fd",
+    "url": "/rohanwedsprashansa/static/media/last.fc32523a.jpeg"
+  },
+  {
+    "revision": "7409d7691ddca34f75b155583093d90e",
+    "url": "/rohanwedsprashansa/static/media/selfies.7409d769.gif"
+  },
+  {
+    "revision": "66ad2dbab57d7ba8fdf3bf96d2e4682d",
+    "url": "/rohanwedsprashansa/static/media/flogo.66ad2dba.png"
+  },
+  {
+    "revision": "28e6635ead6c8d09dd98fb1e56e708d9",
+    "url": "/rohanwedsprashansa/static/media/tick.28e6635e.mp3"
+  },
+  {
+    "revision": "92aea05e365be0390a5132ac203ad861",
+    "url": "/rohanwedsprashansa/static/media/kesariya.92aea05e.mp3"
+  },
+  {
+    "revision": "fb5d1241856a1daa35537ec7de555e51",
+    "url": "/rohanwedsprashansa/index.html"
+  }
+];
